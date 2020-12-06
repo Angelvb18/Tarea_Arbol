bd4c9e0d7699fc7c4d3a7f841d806e6d2fc579c8
Angelvb18 <angeljosevb12@gmail.com>
2020-12-06 15:58:40 -0600

    Menu

---
+++ Tarea_Arbol/src/tarea_arbol/Tarea_Arbol.java
@@ -0,0 +1,45 @@
+
+package tarea_arbol;
+
+import java.util.Scanner;
+
+public class Tarea_Arbol {
+
+     static Scanner lea = new Scanner(System.in);
+    public static void main(String[] args) {
+        Main();
+    }
+    public static void Main(){
+        
+        char opcion_MenuPrincipal = '0' ;
+        while(opcion_MenuPrincipal != 6){
+            System.out.print("1.Cragar Arbol\n"
+                    + "2.Guardar Arbol\n"
+                    + "3.Imprimir Árbol\n"
+                    + "4.Insertar Elemento\n"
+                    + "5.Buscar Elemento\n"
+                    + "6.Salir\n"
+                    + "Ingrese una opcion:");
+            opcion_MenuPrincipal = lea.next().charAt(0);
+            switch (opcion_MenuPrincipal) {
+                case '1':
+                    
+                    break;
+                case '2':
+                    break;
+                case '3':
+                    break;
+                case '4':
+                    break;
+                case '5':
+                    break;
+                case '6':
+                    System.out.println("Adios");
+                    break;
+                default:
+                    System.out.println("Ingreso una opcion no valida");
+                    break;
+            }
+        }
+    }
+}
